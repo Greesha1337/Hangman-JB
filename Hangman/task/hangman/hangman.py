6d409064bfe65ee5af88e2d0c9f0a4cc9ff2572d
Grigory Rodimov <grishanyar@gmail.com>
2020-06-22 18:10:08 +0300

    Hangman stage #2

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,2 +1,7 @@
 # Write your code here
-print("H A N G M A N", "The game will be available soon.", sep='\n')
+print("H A N G M A N", sep='\n')
+user_answer = input("Guess the word: > ")
+if user_answer == "python":
+    print("You survived!")
+else:
+    print("You are hanged!")
