53bef72df3808b8ce13562f074d8138c597788d1
Grigory Rodimov <grishanyar@gmail.com>
2020-06-28 14:04:01 +0300

    Stage #4: Help is on the way

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -2,7 +2,7 @@ import random
 words = ['python', 'java', 'kotlin', 'javascript']
 answer = words[random.randint(0, len(words) - 1)]
 print("H A N G M A N", sep='\n')
-user_answer = input("Guess the word: > ")
+user_answer = input(f'Guess the word {answer[:3] + "-" * len(answer[3:])}: > ')
 if user_answer == answer:
     print("You survived!")
 else:
