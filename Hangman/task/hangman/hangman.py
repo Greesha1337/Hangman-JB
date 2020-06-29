5106e6f8295c3a4fbd7db46dea52034cde998665
Grigory Rodimov <grishanyar@gmail.com>
2020-06-29 22:46:46 +0300

    Hangman stage #6

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -1,7 +1,9 @@
 import random
+
 words = ['python', 'java', 'kotlin', 'javascript']
 try_count = 1
 answer = words[random.randint(0, len(words) - 1)]
+
 print("HANGMAN", sep='\n')
 
 # user_answer = input(f'Guess the word {answer[:3] + "-" * len(answer[3:])}: > ')
@@ -9,25 +11,36 @@ print("HANGMAN", sep='\n')
 #     print("You survived!")
 # else:
 #     print("You are hanged!")
+
 right_answer = list('-' * len(answer))
 
 while try_count <= 8:
     print('\n', *right_answer, sep='')
     user_answer = input('Input a letter: ')
-    if user_answer in answer:
+    if user_answer in right_answer:
+        try_count += 1
+        print('No improvements')
+    elif user_answer in answer:
         for letter in range(len(answer)):
             if answer[letter] == user_answer:
                 right_answer[letter] = user_answer
-            else:
-                continue
     else:
-        print('No such letter in the word\n')
-    try_count += 1
+        try_count += 1
+        print('No such letter in the word')
+
+    if ''.join(right_answer) == answer:
+        print(''.join(right_answer), '''\nYou guessed the word!
+You survived!''')
+        break
+    else:
+        continue
+
+if try_count > 8:
+    print('You are hanged!')
 
 
-
-print('''\nThanks for playing!
-We\'ll see how well you did in the next stage''')
+# print('''\nThanks for playing!
+# We\'ll see how well you did in the next stage''')
 
 
 
