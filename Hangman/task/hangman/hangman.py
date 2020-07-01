f51a19c597a2987a8762f133abae6f31ffd04eb5
Grigory Rodimov <grishanyar@gmail.com>
2020-07-01 15:09:43 +0300

    Hangman stage #8

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -5,50 +5,57 @@ try_count = 1
 answer = words[random.randint(0, len(words) - 1)]
 already_exists_letters = []
 print("HANGMAN", sep='\n')
+while True:
+    user_choise = input("Type \"play\" to play the game, \"exit\" to quit: ")
+    if user_choise == 'play':
 
-# user_answer = input(f'Guess the word {answer[:3] + "-" * len(answer[3:])}: > ')
-# if user_answer == answer:
-#     print("You survived!")
-# else:
-#     print("You are hanged!")
+    # user_answer = input(f'Guess the word {answer[:3] + "-" * len(answer[3:])}: > ')
+    # if user_answer == answer:
+    #     print("You survived!")
+    # else:
+    #     print("You are hanged!")
 
-right_answer = list('-' * len(answer))
+        right_answer = list('-' * len(answer))
 
-while try_count <= 8:
-    while True:
-        print('\n', *right_answer, sep='')
-        user_answer = input('Input a letter: ')
-        if len(user_answer) != 1:
-            print('You should input a single letter')
-        elif user_answer.islower() and user_answer.isascii():
-            break
-        else:
-            print('It is not an ASCII lowercase letter')
+        while try_count <= 8:
+            while True:
+                print('\n', *right_answer, sep='')
+                user_answer = input('Input a letter: ')
 
-    # if user_answer in right_answer:
-    #     try_count += 1
-    #     print('No improvements')
-    if user_answer in already_exists_letters or user_answer in right_answer:
-        print('You already typed this letter')
-    elif user_answer in answer:
-        for letter in range(len(answer)):
-            if answer[letter] == user_answer:
-                right_answer[letter] = user_answer
-    else:
-        already_exists_letters.append(user_answer)
-        try_count += 1
-        print('No such letter in the word')
+                if len(user_answer) != 1:
+                    print('You should input a single letter')
+                elif user_answer.islower() and user_answer.isascii():
+                    break
+                else:
+                    print('It is not an ASCII lowercase letter')
 
-    if ''.join(right_answer) == answer:
-        print(''.join(right_answer), '''\nYou guessed the word!
-You survived!''')
+            # if user_answer in right_answer:
+            #     try_count += 1
+            #     print('No improvements')
+            if user_answer in already_exists_letters or user_answer in right_answer:
+                print('You already typed this letter')
+            elif user_answer in answer:
+                for letter in range(len(answer)):
+                    if answer[letter] == user_answer:
+                        right_answer[letter] = user_answer
+            else:
+                already_exists_letters.append(user_answer)
+                try_count += 1
+                print('No such letter in the word')
+
+            if ''.join(right_answer) == answer:
+                print(''.join(right_answer), '''\nYou guessed the word!
+        You survived!''')
+                break
+            else:
+                continue
+
+        if try_count > 8:
+            print('You are hanged!')
+    if user_choise == 'exit':
         break
     else:
         continue
-
-if try_count > 8:
-    print('You are hanged!')
-
 
 # print('''\nThanks for playing!
 # We\'ll see how well you did in the next stage''')
